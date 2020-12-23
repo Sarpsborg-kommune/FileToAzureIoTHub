d8fd6b858665f701b40a89c7ffa79d389ded2842
Jens Torgeir Solaugsten <jens-torgeir.solaugsten@sarpsborg.com>
2020-12-23 09:46:13 +0100

    Changed to allow all energy types

--- Receivers/SmartElektro.cs
+++ Receivers/SmartElektro.cs
@@ -28,19 +28,33 @@ namespace FileToAzureIoTHub.Receivers
             {
                 string[] item = line.Split(',');
 
-                string matchString1 = "_Total_Active_Energy_Import_T";
-                string matchString2 = "Cluster1.";
-                if (item[1].Contains(matchString1))
-                {
-                    SeData record = new SeData();
-                    record.Timestamp = DateTime.Parse(item[0]).ToUniversalTime();
-                    record.Id = item[1].Replace(matchString1, String.Empty).Replace(matchString2, String.Empty);
-                    record.Value = Convert.ToDouble(item[2], CultureInfo.InvariantCulture);
-                    sedata.Add(record);
-                }
+                string matchString1 = "Cluster1.";
+                string matchString2 = "_Total_Active_Energy_Import_T";
+                string matchString3 = "_kwh_T";
+                string matchString4 = "_ENERGY_T";
+                string matchString5 = "_Accumulated_heat_energy_T";
+                string matchString6 = "_Heat_energy_T";
+
+                SeData record = new SeData();
+                record.Timestamp = DateTime.Parse(item[0]).ToUniversalTime();
+                record.Id = item[1].Replace(matchString1, String.Empty);
+
+                if (record.Id.Contains(matchString2))
+                    record.Id.Replace(matchString2, String.Empty);
+                else if (record.Id.Contains(matchString3))
+                    record.Id.Replace(matchString3, String.Empty);
+                else if (record.Id.Contains(matchString4))
+                    record.Id.Replace(matchString4, String.Empty);
+                else if (record.Id.Contains(matchString5))
+                    record.Id.Replace(matchString5, String.Empty);
+                else if (record.Id.Contains(matchString6))
+                    record.Id.Replace(matchString6, String.Empty);
+
+
+                record.Value = Convert.ToDouble(item[2], CultureInfo.InvariantCulture);
+                sedata.Add(record);
+
             }
         }
     }
-
-
 }
